0b7c448caa343daf4b5dfc06ad36152e426bf12a
Serge Modin <modinsv@yandex.ru>
2021-04-04 19:19:48 +0300

    Filtering Logic

--- repositories/users.js
+++ repositories/users.js
@@ -63,11 +63,29 @@ class UserRepository {
     Object.assign(record, attrs);
     await this.writeAll(records);
   }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+      if (found) {
+        return record;
+      }
+    }
+  }
 }
 
 const test = async () => {
   const repo = new UserRepository("users.json");
-  await repo.update("cf027931", { password: "mypassword" });
+  const user = await repo.getOneBy({ email: "test@test.ru" });
+  console.log(user);
 };
 
 test();
