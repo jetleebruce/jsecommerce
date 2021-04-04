eb5d268ff078672ac3376a0b1ecfa408c23560a4
Serge Modin <modinsv@yandex.ru>
2021-04-04 14:57:37 +0300

    Form Submition

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hi there!')
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+  <div>
+  <form method="POST">
+  <input name="email" placeholder="email">
+  <input name="password" placeholder="password">
+  <input name="passwordConfirmation" placeholder="password confirmation">
+  <button>Sign Up</button>
+  </form>
+  </div>
+    `);
+});
+
+app.post("/", (req, res) => {
+  res.send("Account created!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening");
+});
