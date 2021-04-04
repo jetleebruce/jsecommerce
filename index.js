5451e9bb9a0ecde2a2ee009b4c9bb200e7869490
Serge Modin <modinsv@yandex.ru>
2021-04-04 15:58:05 +0300

    Globally applying bodyParser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
@@ -16,16 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  // get access to email, password, passwordConfirmation
-  req.on("data", (data) => {
-    const parsed = data.toString("utf8").split("&");
-    const formData = {};
-    for (let pair of parsed) {
-      const [key, value] = pair.split("=");
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+  console.log(req.body);
   res.send("Account created!");
 });
 
