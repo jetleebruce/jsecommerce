f495e5216eebe2102280751a2459fc81d14f2c53
Serge Modin <modinsv@yandex.ru>
2021-04-06 10:03:10 +0300

    Fetching a session

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users");
-const users = require("./repositories/users");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["45665455544558855"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send(`
   <div>
+  You id is ${req.session.userId}
   <form method="POST">
   <input name="email" placeholder="email">
   <input name="password" placeholder="password">
@@ -30,10 +36,11 @@ app.post("/", async (req, res) => {
   if (password !== passwordConfirmation) {
     return res.send("Password must match");
   }
-// create a usre in our repo to represent this person
+  // create a usre in our repo to represent this person
   const user = await usersRepo.create({ email, password });
-// Store the id that user inside the users cookie
-  
+  // Store the id that user inside the users cookie
+  req.session.userId = user.id;
+
   res.send("Account created!");
 });
 
