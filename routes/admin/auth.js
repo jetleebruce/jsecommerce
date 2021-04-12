2bd1508aa7bb1352451549baeafa6fc785058fd5
Serge Modin <modinsv@yandex.ru>
2021-04-12 09:01:50 +0300

    Extracting Validation Chains

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -4,6 +4,11 @@ const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
 
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+} = require("./validators");
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
@@ -12,23 +17,12 @@ router.get("/signup", (req, res) => {
 
 router.post(
   "/signup",
-  [
-    check("email").trim().normalizeEmail().isEmail(),
-    check("password").trim().isLength({ min: 4, max: 20 }),
-    check("passwordConfirmation").trim().isLength({ min: 4, max: 20 }),
-  ],
+  [requireEmail, requirePassword, requirePasswordConfirmation],
   async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
     const { email, password, passwordConfirmation } = req.body;
 
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-      return res.send("Email in use");
-    }
-    if (password !== passwordConfirmation) {
-      return res.send("Password must match");
-    }
     // create a usre in our repo to represent this person
     const user = await usersRepo.create({ email, password });
     // Store the id that user inside the users cookie
